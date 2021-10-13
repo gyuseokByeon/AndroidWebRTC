2da9136494a999e1408aabf1df7759b6db1e97eb
gyuseokByeon <39879195+gyuseokByeon@users.noreply.github.com>
2021-10-13 09:09:31 +0900

    romm 변경

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var sdpConstraints = {
 
 /////////////////////////////////////////////
 
-var room = 'foo';
+var room = 'drone_live';
 // Could prompt for room name:
 // room = prompt('Enter room name:');
 
